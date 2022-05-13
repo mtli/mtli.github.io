b683ad15ec9ef685feb220b33848e3a684213b4b
mt <martinli.work@gmail.com>
2022-05-12 22:44:06 -0400

    Migrate website here from cs.cmu.edu

---
+++ [js]/main.js
@@ -0,0 +1,9 @@
+$(function() {
+    "use strict";
+   
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+      $('.navbar-collapse').collapse('hide');
+    });
+  });
+  
